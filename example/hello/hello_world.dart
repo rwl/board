f23f1d618e9be7d397ac7c03ce48cb40c67dc098
Richard Lincoln <r.w.lincoln@gmail.com>
2014-06-06 22:22:58 +0200

    Adding HTML wrapper for hello world example.

--- example/hello/hello_world.dart
+++ example/hello/hello_world.dart
@@ -1,34 +1,27 @@
-
 import 'dart:html';
-import 'package:dgraph/swing/swing.dart';
 import 'package:dgraph/view/view.dart';
 
 
-main()  {
+main() {
 
   final container = document.querySelector("#graph");
 
   Graph graph = new Graph(container);
 
-//  new Rubberband(graph);
+  //  new Rubberband(graph);
 
   Object parent = graph.getDefaultParent();
 
   graph.getModel().beginUpdate();
-  try
-  {
-      Object v1 = graph.insertVertex(parent, null, "Hello", 20.0, 20.0, 80.0,
-              30.0);
-      Object v2 = graph.insertVertex(parent, null, "World!", 240.0, 150.0,
-              80.0, 30.0);
-      graph.insertEdge(parent, null, "Edge", v1, v2);
-  }
-  finally
-  {
-      graph.getModel().endUpdate();
+  try {
+    Object v1 = graph.insertVertex(parent, null, "Hello", 20.0, 20.0, 80.0, 30.0);
+    Object v2 = graph.insertVertex(parent, null, "World!", 240.0, 150.0, 80.0, 30.0);
+    graph.insertEdge(parent, null, "Edge", v1, v2);
+  } finally {
+    graph.getModel().endUpdate();
   }
 
-//  GraphComponent graphComponent = new GraphComponent(graph);
-//  getContentPane().add(graphComponent);
+  //  GraphComponent graphComponent = new GraphComponent(graph);
+  //  getContentPane().add(graphComponent);
 
 }
